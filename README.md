42636a7fcf5b2b5ef0338b0d26546a314aec160a
Andreas Scheucher <ans@dev.walou.ag>
2020-12-03 15:16:49 +0100

    first working version.

--- README.md
+++ README.md
@@ -11,4 +11,23 @@ npm install -g vsce
 vsce package
 ```
 
-Take a look into the DesignDb .devcontainer/devcontainer.json file on how to install it then.
+## install it in a Docker devcontainer VSC project
+
+In the .devcontainer/devcontainer.json add following;
+
+```JSON
+    ...
+    // Add the IDs of extensions you want installed when the container is created.
+    "extensions": [
+    "/tmp/env-monitor-0.0.1.vsix"
+    ],
+    ...
+```
+
+In the .devcontainer/Dockerfile add:
+
+```Dockerfile
+COPY env-monitor-0.0.1.vsix /tmp/
+```
+
+This is a bit hacky, but it works for now.
--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,18 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import { env } from "process";
-import { print } from "util";
+import * as fs from "fs";
 import * as vscode from "vscode";
 
 let myStatusBarItem: vscode.StatusBarItem;
 
-export function activate( context:  vscode.ExtensionContext) {
-
+export function activate(context: vscode.ExtensionContext) {
   // register a command that is invoked when the status bar
   // item is selected
   const myCommandId = "ascheucher.showEnvironment";
   context.subscriptions.push(
     vscode.commands.registerCommand(myCommandId, () => {
-      vscode.window.showInformationMessage(getEnviroMessage());
+      updateStatusBarItem();
     })
   );
 
@@ -27,33 +25,48 @@ export function activate( context:  vscode.ExtensionContext) {
   myStatusBarItem.command = myCommandId;
   context.subscriptions.push(myStatusBarItem);
 
-  // register some listener that make sure the status bar
-  // item always up-to-date
-  context.subscriptions.push(
-    vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem)
-  );
-  context.subscriptions.push(
-    vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem)
-  );
-
   // update status bar item once at start
   updateStatusBarItem();
 }
 
 function updateStatusBarItem(): void {
-  myStatusBarItem.text = getEnviroMessage();
+  const msg = getEnviroMessage();
+  vscode.window.showInformationMessage(msg);
+  myStatusBarItem.text = msg;
   myStatusBarItem.show();
 }
 
 function getEnviroMessage(): string {
-  // https://stackoverflow.com/questions/65080704/access-to-env-variables-defined-in-workspacefolder-env-files
-  const path = process.env["PATH"];
-  const envValue = process.env["ENVIRO"];
-  vscode.window.showInformationMessage(`ENVIRO: ${envValue}`);
-  
-  if (envValue == "prod") {
-    return `$(zap) ENVIRO:  ${envValue}`;
-  } else {
-    return `$(flame) ENVIRO:  ${envValue}`;
+  const envValue = getEnviroFromEnvFile();
+  return `ENVIRO:  ${envValue}`;
+}
+
+function getEnviroFromEnvFile(): string {
+  try {
+    let enviro = "No .env file.";
+    if (vscode.workspace.workspaceFolders) {
+      for (const envUri of vscode.workspace.workspaceFolders) {
+        const envFilePath = `${envUri.uri.path}/.env`;
+
+        if (fs.existsSync(envFilePath)) {
+          enviro = "is not set in .env file.";
+          const data = fs.readFileSync(envFilePath, "UTF-8");
+          const lines = data.split(/\r?\n/);
+          lines.forEach((l) => {
+            const tokens = l.split("=");
+            if (tokens.length > 1) {
+              const envVarName = tokens[0].trim();
+              if (envVarName === "ENVIRO") {
+                enviro = tokens[1].trim();
+              }
+            }
+          });
+          return enviro;
+        }
+      }
+    }
+    return enviro;
+  } catch (e) {
+    return ".env file parsig ERR.";
   }
 }
